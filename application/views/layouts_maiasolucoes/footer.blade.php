a9a978e6b87f4481dacc6df116b2d20d20967fd3
raphael <raphael.wcosta@gmail.com>
2013-02-04 11:15:40 -0200

    alterado arquivo footer

--- application/views/layouts_maiasolucoes/footer.blade.php
+++ application/views/layouts_maiasolucoes/footer.blade.php
@@ -1,7 +1,25 @@
-<div id="footer"></div>
-
-<div id="inside_footer">
-
-    Todos os direitos reservados. Copyright &copy; 2013.
-
+<div class="footer">
+    <div class="row-fluid show-grid">
+        <div class="span12">
+            <div class="row-fluid">
+                <div class="span4">
+                    <!--Mantenha contato
+                    <div class="row-fluid">
+                        <div class="span2">
+                            <a href=""><img src="img/icone_facebook.png" width="36" height="36"></a>
+                        </div>
+                        <div class="span2">
+                            <a href=""><img src="img/icone_twitter.png" width="36" heigth="36"></a>
+                        </div>
+                    </div> -->
+                </div>
+                <div class="span4">
+                    Todos os direitos reservados. &copy; 2013
+                </div>
+                <div class="span2 offset2">
+                    <a href="/">Termos de servi&ccedil;os</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
