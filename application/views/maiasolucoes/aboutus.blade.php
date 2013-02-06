ed0b4cabb143c73ad94a6cf144c7f3a7630343bc
Matheus Moraes Negrão Gomes <matheus@maiasolucoes.com.br>
2013-02-06 10:53:15 -0200

    22 ingles

---
+++ application/views/maiasolucoes/aboutus.blade.php
@@ -0,0 +1,41 @@
+@layout('layouts_maiasolucoes/default')
+
+@section('conteudo')
+
+	<div id="box">
+
+		<div class="p box1">
+	
+			<h1>Ricardo Maia</h1>
+			<p class="clear">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			 Etiam ligula lacus, mollis ultrices fringilla ac, sodales et odio. 
+			 Proin nec velit id neque venenatis lobortis. Quisque cursus dignissim est, sit amet gravida augue commodo vel. 
+			 Nam laoreet metus non nisi ullamcorper aliquam. Maecenas elementum erat ut ligula varius tincidunt.</p>
+
+		</div>
+		<div class="p box1">
+	
+			<h1>Raphael Willian</h1>
+			<p class="clear">Fusce egestas, dolor sed suscipit dignissim, enim nunc sodales risus, at congue turpis lacus eu felis.
+			Nulla facilisis cursus dui, ac gravida nunc aliquet vitae. 
+			Ut auctor velit quis purus lobortis nec lobortis ipsum fermentum.</p>
+
+		</div>
+		<div class="p box1">
+	
+			<h1>Cícero Alves</h1>
+			<p class="clear">Vivamus ante mi, porttitor et suscipit ut, pellentesque vel lectus.
+			 Aliquam erat volutpat. In mattis dignissim justo ut facilisis. Nunc ante lacus, condimentum a viverra id, luctus eu lorem. </p>
+
+		</div>
+		<div class="p box1">
+	
+			<h1>Matheus Moraes</h1>
+			<p class="clear">Sou estagiário na empresa. Vivamus ante mi, porttitor et suscipit ut, pellentesque vel lectus. 
+			Aliquam erat volutpat. In mattis dignissim justo ut facilisis. Nunc ante lacus, condimentum a viverra id, luctus eu lorem. </p>
+
+		</div>
+	</div>
+	<div class="clear"></div>
+
+@endsection
