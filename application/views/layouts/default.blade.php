15775639533de166d1bfb195fd624553fd72379e
Cicero <cicero@maiasolucoes.com.br>
2013-02-05 23:09:22 -0200

    Finalizacao do header

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -1,11 +1,8 @@
-
 <html>
 <head>
 
     <meta charset="utf-8">
     <title>title</title>
-    <link href="../assets/css/bootstrap.css" rel="stylesheet">
-    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet' type='text/css'>
     {{ Asset::container('bootstrapper')->styles(); }}
     {{ Asset::styles(); }}
@@ -14,32 +11,8 @@
 
 <body>
 
-<div class="navbar" id="header">
-    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-    </a>
+	@include('layouts/header')
 
-    <img src="img/logo.png">
-
-    <div class="nav-collapse collapse" id="menu">
-        <ul class="nav">
-            <li><a href="principal">Principal</a></li>
-            <li><a href="sobrenos">Sobre Nós</a></li>
-            <li><a href="solucoes">Soluções</a></li>
-            <li><a href="contato">Contato</a></li>
-        </ul>
-    </div>
-</div>
-
-
-
-
-	
-	<!--@include('layouts/header')-->
-
-    <br><br><br><br><br><br><br><br><br><br><br><br>
 	<div id="content">
 
 		@yield('banner')
@@ -47,7 +20,7 @@
 
 	</div>
 
-	@include('layouts/footer')
+	<!--@include('layouts/footer')-->
     {{ Asset::container('bootstrapper')->scripts(); }}
 
 </body>
--- application/views/layouts/header.blade.php
+++ application/views/layouts/header.blade.php
@@ -1,128 +1,19 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--<div class="navbar navbar-inverse navbar-fixed-top" id="header">
+<div class="navbar" id="header">
     <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
     </a>
-    <div id="inside_header">
-        <img src="img/logo.png">
 
-        <div class="nav-collapse collapse" id="menu">
-            <ul class="nav">
-                <li><a href="principal">Principal</a></li>
-                <li><a href="sobrenos">Sobre Nós</a></li>
-                <li><a href="solucoes">Soluções</a></li>
-                <li><a href="contato">Contato</a></li>
-            </ul>
-        </div>
-    </div>
+    <img src="img/logo.png">
 
-</div>-->
-
-
-
-
-
-
-
-
-<!--esse é o exemplo do navbar-->
-<!--<div class="navbar navbar-inverse navbar-fixed-top">
-    <div class="navbar-inner">
-        <div class="container">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </a>
-            <a class="brand" href="#">Project qqqname</a>
-            <div class="nav-collapse collapse">
-                <ul class="nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-
-            </div>
-        </div>
+    <div class="nav-collapse collapse" id="menu">
+        <ul class="nav">
+            <li><a href="principal">Principal</a></li>
+            <li><a href="sobrenos">Sobre Nós</a></li>
+            <li><a href="solucoes">Soluções</a></li>
+            <li><a href="contato">Contato</a></li>
+        </ul>
     </div>
 </div>
-    -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--<div class="navbar" id="header">
-
-    <div class="navbar-inner">
-        <div class="container">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </a>
-            <div id="inside_header">
-
-                <img src="img/logo.png" alt="logo">
-
-                <div class="nav-collapse collapse" id="menu">
-                    <ul class="nav">
-                        <li><a href="contato">Contato</a></li>
-                        <li><a href="solucoes">Soluções</a></li>
-                        <li><a href="sobrenos">Sobre Nós</a></li>
-                        <li><a href="principal">Principal</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</div>-->
-
-<!--<div id="header">
-
-    <div id="inside_header">
-
-        <img src="img/logo.png" alt="logo">
-
-        <div id="menu">
-
-            <ul>
-                <li><a href="contato">Contato</a></li>
-                <li><a href="solucoes">Soluções</a></li>
-                <li><a href="sobrenos">Sobre Nós</a></li>
-                <li><a href="principal">Principal</a></li>
-            </ul>
-
-        </div>
-
-    </div>
 
