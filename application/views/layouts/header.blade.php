853f497b00124b74c4443e85866d25f0d856b7dc
Cicero <cicero@maiasolucoes.com.br>
2013-02-21 11:51:13 -0300

    Merge branch '35'
    Conflicts:
    	application/views/layouts/header.blade.php

--- application/views/layouts/header.blade.php
+++ application/views/layouts/header.blade.php
@@ -11,11 +11,13 @@
     <div class="nav-collapse collapse" id="menu">
 
         <ul class="nav">
+
             <li><a href="../">Principal</a></li>
             <li><a href="../about">Sobre Nós</a></li>
             <li><a href="../solutions">Soluções</a></li>
             <li><a href="../contact">Contato</a></li>
             <li class="active">@yield('logout')</li>
+
         </ul>
 
     </div>
