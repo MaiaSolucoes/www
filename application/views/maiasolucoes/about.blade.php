6cbfea0f9a32a227db6bbe91047827d4912b03c7
Ricardo Maia <maiams@msn.com>
2013-02-22 01:41:02 -0300

    typo

--- application/views/maiasolucoes/about.blade.php
+++ application/views/maiasolucoes/about.blade.php
@@ -14,36 +14,38 @@
 
 		<div class="p box1">
 	
-			<h1>Ricardo Maia</h1>
-			<p class="clear">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-			 Etiam ligula lacus, mollis ultrices fringilla ac, sodales et odio. 
-			 Proin nec velit id neque venenatis lobortis. Quisque cursus dignissim est, sit amet gravida augue commodo vel. 
-			 Nam laoreet metus non nisi ullamcorper aliquam. Maecenas elementum erat ut ligula varius tincidunt.</p>
+			<h4>Nossa proposta</h4>
+			<p class="clear">
+				Somos uma pequena empresa e desenvolvemos soluções para os pequenos como nós. Nós sabemos o quanto
+				somos importantes para a economia do país e também sabemos que os grandes softwares de gestão são muito
+				caros.
+				<br />
+				Pensando nisso, criamos ferramentas simples, diretas e com uma linguagem próxima do cliente ao invés de
+				telas complicadas e cheias de informações desnecessárias. Acreditamos que a tecnologia está a serviço
+				das pessoas e dos negócios.
+				<br />
+				Estamos envolvidos em transformar a TI (Tecnologia da Informação) em TN (Tecnologia dos Negócios). Para
+				isso, utilizamos boas práticas do mercado e as transmitimos aos nossos clientes na forma de serviço.
+			</p>
+			<p class="clear">
+				<strong>Missão</strong>
+				<br />
+				Desenvolver soluções inteligentes, maximizando valor para nossos clientes.
+				<strong>Visão</strong>
+				<br />
+				Ser referência no Brasil em qualidade de software, preço baixo e inovação, através do investimento
+				constante em pessoas e novas tecnologias.
+				<strong>Valores</strong>
+				<br />
+				Ter uma conduta ética, honesta e transparente dentro e fora da empresa.
+				<br />
+				Ter foco e objetivo, priorizando resultados.
+				<br />
+				Ter respeito pelas pessoas e pelo meio ambiente.
+				<br />
+				Ter uma política de gestão em equipe.
 
-		</div>
-
-		<div class="p box1">
-	
-			<h1>Raphael Willian</h1>
-			<p class="clear">Fusce egestas, dolor sed suscipit dignissim, enim nunc sodales risus, at congue turpis lacus eu felis.
-			Nulla facilisis cursus dui, ac gravida nunc aliquet vitae. 
-			Ut auctor velit quis purus lobortis nec lobortis ipsum fermentum.</p>
-
-		</div>
-
-		<div class="p box1">
-	
-			<h1>Cícero Alves</h1>
-			<p class="clear">Vivamus ante mi, porttitor et suscipit ut, pellentesque vel lectus.
-			 Aliquam erat volutpat. In mattis dignissim justo ut facilisis. Nunc ante lacus, condimentum a viverra id, luctus eu lorem. </p>
-
-		</div>
-
-		<div class="p box1">
-	
-			<h1>Matheus Moraes</h1>
-			<p class="clear">Sou estagiário na empresa. Vivamus ante mi, porttitor et suscipit ut, pellentesque vel lectus. 
-			Aliquam erat volutpat. In mattis dignissim justo ut facilisis. Nunc ante lacus, condimentum a viverra id, luctus eu lorem. </p>
+			</p>
 
 		</div>
 
