7199fa5990799207c2c907e612c88e55c61ab795
Ricardo Maia <maiams@msn.com>
2013-02-22 00:34:49 -0300

    Revert "undo"
    This reverts commit 79f1407474ae77aa6252ff71837831e9ed9405bd.

--- application/views/maiasolucoes/solutions.blade.php
+++ application/views/maiasolucoes/solutions.blade.php
@@ -12,61 +12,36 @@
 
 		<div class="p box1">
 	
-			<h1>Seu negócio no caminho certo</h1>
-			<p class="clear">
-				Nós trabalhamos duro para entregar a você as ferramentas necessárias para tornar a sua empresa mais
-				competitiva e capaz de tomar decisões com mais velocidade. Nossa ferramenta de micro-ERP permite
-				armazenar e recuperar todas as informações da sua empresa em um modelo simplificado. Tenha sempre em
-				mãos o controle do seu negócio, seja no trabalho, em casa ou no seu celular.
-			</p>
+			<h1>Exemplo 1</h1>
+			<p class="clear">Enim nunc sodales risus,  at congue turpis lacus eu felis.
+			Nulla facilisis cursus dui, ac gravida nunc aliquet vitae.
+			AA congue turpis lacus eu felis.
+			Nulla facilisis cursus dui, ac gravida nunc aliquet vitae.</p>
 
 		</div>
 
 		<div class="p box1">
 	
-			<h1>Aproxime-se dos seus clientes</h1>
-			<p class="clear">
-				Nossa ferramenta de relacionamento com o cliente aproxima sua você dos seus clientes aonde eles
-				estiverem. Crie campanhas de marketing personalizadas de acordo com o perfil dos seus clientes e
-				acompanhe os resultados em tempo real. Envie mensagens de e-mail ou SMS automaticamente em ocasiões
-				especiais como aniversários ou datas importantes. Mantenha contato e veja o que seus clientes dizem
-				dos seus produtos nas redes sociais. Tudo em uma ferramenta simples, direta e elegante.
-
-			</p>
-
-		</div>
-
-		<div class="p box1">
-
-			<h1>Apenas o necessário</h1>
-			<p class="clear">
-				Nós não gostamos de telas de cadastro sem sentido e de sistemas difíceis de se entender. Temos a
-				preocupação de entregar ferramentas realmente úteis e fáceis de se usar. Nossa filosofia é simples:
-				"faça apenas o necessário e bem feito".
-			</p>
-
-		</div>
-
-		<div class="p box1">
-
-			<h1>Pague somente o que usar</h1>
-			<p class="clear">
-				Nossos contratos não possuem tempo mínimo de permanência e todos os serviços são ofertados sob demanda.
-				Isso significa que você paga uma pequena quantia mensal pelo que utilizar. Recursos não utilizados não
-				são cobrados e não existe uma taxa mínima para utilização de um recurso.
-			</p>
+			<h1>Exemplo 2</h1>
+			<p class="clear">Fusce egestas, dolor sed suscipit dignissim, enim nunc sodales risus, at congue turpis lacus eu felis.
+			Nulla facilisis cursus dui, ac gravida nunc aliquet vitae. 
+			Ut auctor velit quis purus lobortis nec lobortis ipsum fermentum.</p>
 
 		</div>
 
 		<div class="p box1">
 	
-			<h1>Aumente sua presença</h1>
-			<p class="clear">
-				Esteja online 24 horas por dia, 7 dias por semana e exponha o seu negócio na Internet. Temos uma
-				ferramenta simples o suficiente para ser usada por leigos e que permite construir seu site em minutos.
-				Se desejar, temos soluções personalizadas para vendas online, tudo integrado com as ferramentas que
-				disponibilizamos.
-			</p>
+			<h1>Exemplo 3 </h1>
+			<p class="clear">Vivamus ante mi, porttitor et suscipit ut, pellentesque vel lectus.
+			 Aliquam erat volutpat. In mattis dignissim justo ut facilisis. Nunc ante lacus, condimentum a viverra id, luctus eu lorem. </p>
+
+		</div>
+
+		<div class="p box1">
+	
+			<h1>Exemplo 4</h1>
+			<p class="clear">Sou estagiário na empresa. Vivamus ante mi, porttitor et suscipit ut, pellentesque vel lectus. 
+			Aliquam erat volutpat. In mattis dignissim justo ut facilisis. Nunc ante lacus, condimentum a viverra id, luctus eu lorem. </p>
 
 		</div>
 
