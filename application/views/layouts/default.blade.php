e2e3d2b163898a5c5d769b2c28c7be99267f35d0
Ricardo Maia <maiams@msn.com>
2013-02-22 17:44:13 -0300

    meta tags

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -1,40 +1,45 @@
 <html>
 <head>
 
-    <meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<meta name="description"
+		  content="Temos uma única paixão: Ajudar nossos clientes a desenvolver seu potencial. Nós fazemos isso criando soluções intuitivas e que funcionam de verdade."/>
+	<meta name="keywords"
+		  content="tecnologia, internet, site, criacao de site, erp, crm, relacionamento, cliente, tecnologia, php, web, empresa, sjc, software"/>
+	<meta http-equiv="Content-Language" content="pt-br"/>
 
-    <title>@yield('title')</title>
+	<title>@yield('title')</title>
 
-    <link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet' type='text/css'>
-    <link rel='shortcut icon' href='/favicon.png' />
-    {{ Asset::container('bootstrapper')->styles(); }}
-    {{ Asset::styles(); }}
+	<link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet' type='text/css'>
+	<link rel='shortcut icon' href='/favicon.png'/>
+	{{ Asset::container('bootstrapper')->styles(); }}
+	{{ Asset::styles(); }}
 
-    <script type="text/javascript">
-        $(function(){
-            $('.alert').alert('close');
-        });
+	<script type="text/javascript">
+		$(function () {
+			$('.alert').alert('close');
+		});
 
-    </script>
+	</script>
 
 </head>
 
 <body>
-	<div id="all">
-		@include('layouts/header')
+<div id="all">
+	@include('layouts/header')
 
-		<div id="content">
+	<div id="content">
 
-			@yield('banner')
+		@yield('banner')
 
-			@yield('content')
+		@yield('content')
 
-		</div>
-		<div id="footer">
-			@include('layouts/footer')
-		</div>
 	</div>
-    {{ Asset::container('bootstrapper')->scripts(); }}
+	<div id="footer">
+		@include('layouts/footer')
+	</div>
+</div>
+{{ Asset::container('bootstrapper')->scripts(); }}
 
 </body>
 
