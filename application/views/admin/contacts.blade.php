15dbcdb417989ccf3e1cfd11e98c9625002012a1
Ricardo Maia <maiams@msn.com>
2013-02-26 00:00:38 -0300

    wordwrap

--- application/views/admin/contacts.blade.php
+++ application/views/admin/contacts.blade.php
@@ -40,7 +40,7 @@
                 <tr>
                     <td>{{ $valor->name }}</td>
                     <td>{{ $valor->email }}</td>
-                    <td>{{ wordwrap(Str::limit($valor->message, 2048), 80, "<br />\n") }}</td>
+                    <td>{{ wordwrap(Str::limit($valor->message, 2048), 80, "<br />\n", true) }}</td>
                     <td>{{ $valor->ip }}</td>
                     <td>{{ $valor->created_at }}</td>
                 </tr>
