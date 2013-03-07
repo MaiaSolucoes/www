be0717465ed9c503a7766d91c93595da68b67657
Ricardo Maia <maiams@msn.com>
2013-03-07 09:43:57 -0300

    start.php

---
+++ bundles/httpful/start.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ricardo
+ * Date: 07/03/13
+ * Time: 09:36
+ * To change this template use File | Settings | File Templates.
+ */
+
+include 'src'.DS.'Httpful'.DS.'start.php';
