1726cfed8efef53da9d1c4d9670b132f4179df3d
Maia <maiams@msn.com>
2013-03-14 11:04:45 -0300

    footer fix

--- application/views/layouts/footer.blade.php
+++ application/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="container">
+<footer id="footer" class="container">
     <div class="row downset1">
         <div class="span3 offset4">Todos os direitos reservados. &copy; 2013</div>
         <div class="span2 offset3"><a href="/terms">Termos de Servi&ccedil;os</a></div>
