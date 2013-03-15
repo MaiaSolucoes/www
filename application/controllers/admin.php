29525361f7b5b4438728727ae4f3875978b0bc95
Cicero <cicero@maiasolucoes.com.br>
2013-03-15 20:52:35 -0300

    99% completo

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -2,12 +2,16 @@
 
 class Admin_Controller extends Base_Controller {
 
+    public static $cache_token;
+
+    public static $cache_user;
+
 	public function action_index() {
 
         //return Pulsar::check() ? Pulsar::check().'truee' : 'falseee';
         if(Pulsar::check()){
 
-            return 'admin check ON';
+            //return 'admin check ON';
             return Redirect::to('../admin/contacts');
 
         }
@@ -27,23 +31,38 @@ class Admin_Controller extends Base_Controller {
 
             if(Pulsar::check()) {
 
-                return 'admin check login ONN';
+                //return 'admin check login ONN';
                 return Redirect::to('../admin/contacts');
 
             }
 
-            return 'admin check login OFF';
+            //return 'admin check login OFF';
             return Redirect::to('../admin');
 
         } else {
 
 
             $token = Pulsar::login(array('username' => $input['email'], 'password' => $input['password']));
-            return $token;
-            $user = Pulsar::who();
+            //return dd(($token)).'aki';
+            //$user = Pulsar::who();
             //return Redirect::to('../admin/contacts/')->with('token', $token, 'user', $user);
-            return Pulsar::check() ? 'truee' : 'falseee';
-            return 'admin check login ON foi';
+            //return dd(Pulsar::check()).'pol';// ? 'truee' : 'falseee';
+            if(!$token){
+                //? Pulsar::$cache_user.'  =user     '.Pulsar::$cache_token.' =token'
+                return dd(Pulsar::check()).'pol';// ? 'truee' : 'falseee';
+                //return Redirect::to('../admin');
+            } else {
+                self::$cache_token = $token;
+                self::$cache_user = $input['email'];
+                //return dd(Pulsar::check().'posdfsfsdl');// ? 'truee' : 'falseee';
+                return Redirect::to('../admin/contacts');
+
+            }
+
+
+
+
+            //return 'admin check login ON foi';
             //return Redirect::to('../admin/contacts')->with(array('username' => $username, 'token' => $token));
 
         }
@@ -52,17 +71,22 @@ class Admin_Controller extends Base_Controller {
 
     public function action_contacts($page=1) {
 
+        //return 'user='.self::$cache_user.'       tk= '.self::$cache_token;
+
+        //return Pulsar::$cache_user.'  =user     '.Pulsar::$cache_token.' =token';
+        //return dd(Pulsar::check());// ? 'truee' : 'falseee';
+
         if(!Pulsar::check()) {
 
-            return 'admin check contact OFF';
+            //return 'admin check contact OFFFF';
             return Redirect::to('../admin');
 
         }
-        $user = Pulsar::who();
+        $user = Cache::get('username');
         $per_page = 15;
         $messages = DB::table('contacts')->paginate($per_page, array('id', 'name','email','message','ip', 'created_at'));
 
-        return 'admin check contact ON';
+        //return 'admin check contact ONdgdfgdf';
 		return View::make('admin.contacts')->with(array('messages' => $messages,'page' => $page, 'user' => $user));
 
     }
--- bundles/pulsar/pulsar.php
+++ bundles/pulsar/pulsar.php
@@ -2,7 +2,6 @@
 
 class Pulsar {
 
-    public static $token;
     // DEFAULT URL PULSAR
     public static $url = "http://pul.cicero.maiasolucoes.com.br";
 
@@ -13,24 +12,21 @@ class Pulsar {
     }
     public static function login ($arguments = array()){
         if (empty($arguments['username']) or empty($arguments['password'])){
-            return "Empty Fields";
+            return false;
         }
         $url = self::$url . "/auth/login?username=" . $arguments['username'] . "&password=" . $arguments['password'];
-        $status = self::status($url);
-        switch($status){
-            case "200":
-                $data = self::prepare($url);
-                self::$token = $data;
-                break;
-            case "404":
-                $data = "Not found";
-                break;
-            default:
-                $data = "Error";
+        $prepare = self::prepare($url);
+        if(is_null($prepare->$arguments['username'])){
+
+             return false;
+
+        } else {
+
+            Cache::put('username',$arguments['username'],1);
+            Cache::put('token',$prepare->$arguments['username'],1);
+            return true;
+
         }
-
-
-        return $data->$arguments['username'];
 
     }
 
@@ -40,7 +36,8 @@ class Pulsar {
 		if(!is_array($result)) {
 			return "Not found";
 		}
-        list($username, $token) = $result;
+        $username = $result['username'];
+        $token = $result['token'];
         $data = self::prepare(self::$url . "/user/user?username=$username&token=$token");
         return $data;
     }
@@ -48,12 +45,14 @@ class Pulsar {
     public static function check() {
 
         $result = self::get_user_token();
+        //return $result;
         if(!is_array($result)) {
             return false;
         }
-        list($username, $token) = $result;
+        $username = $result['username'];
+        $token = $result['token'];
         $data = self::prepare(self::$url . "/auth/check?username=$username&token=$token");
-        return 'data='.$data;
+        return $data;
     }
 
     public static function logout() {
@@ -61,7 +60,8 @@ class Pulsar {
         if(!is_array($result)) {
             return "Not found";
         }
-        list($username, $token) = $result;
+        $username = $result['username'];
+        $token = $result['token'];
         $content = self::prepare(self::$url . "/auth/logout?username=$username&token=$token");
         return $content;
     }
@@ -73,23 +73,21 @@ class Pulsar {
     }
 
     public static function get_user_token() {
-        /*Session::get('username') != null ? $username = Session::get('username') : $username = null ;
-        Session::get('token') != null ? $token = Session::get('token') : $token = null ;*/
 
-		$username = Session::get('username') != null
-			? Session::get('username')
-			: null;
-		$token = Session::get('token') != null
-			? Session::get('token')
-			: null;
+        $username = Cache::has('username') ? Cache::get('username') : null;
+        $token = Cache::has('token') ? Cache::get('token') : null;
 
         if(is_null($username) or is_null($token)) {
+
             return false;
+
         } else {
+
 			return array(
 				'username' => $username,
 				'token' => $token
 			);
+
         }
 
     }
