d73267ca88529e895d9554dc76266f0ace1a3423
Cícero Alves S. Junior <cicerocasj@gmail.com>
2013-04-12 12:51:21 -0300

    instalando Google Analytics

--- application/views/maiasolucoes/index.blade.php
+++ application/views/maiasolucoes/index.blade.php
@@ -8,7 +8,7 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-40087868-1', 'maiasolucoes.com.br');
+    ga('create', 'UA-40076393-1', 'maiasolucoes.com.br');
     ga('send', 'pageview');
 
 </script>
